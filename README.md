02b5ce1e9eba78d36b36b25bd97ab84e2324dd58
Nilet <nilet@tutanota.com>
2022-04-09 14:20:09 -0300

    Works on my machine

---
+++ README.md
@@ -0,0 +1,44 @@
+# i3currentMedia
+
+![screen](./res/soundcloud_scr.png)
+
+### About:
+i3currentMedia is a fork from [@rpieja i3spotifystatus](https://github.com/rpieja/i3spotifystatus) which i recommend to check it out, that uses `playerctl` to get the current media playing from any source, not only spotify, made for myself but it might be what someone else wants.
+
+i3currentMedia is a tiny python script that parses JSON outputed by i3status, adds information about song author and title and outputs it to i3bar.
+
+### What you'll need:
+* playerctl
+	if you are on an Arch based distro you can get it with:
+	`sudo pacman -S playerctl`
+
+### How to install:
+
+* clone repository to your prefered location
+* in your i3 config file (usually placed in ~/.config/i3/) set `status_command` to `i3status | /path/to/your/pystatus.py` in `bar` section, like this:
+
+```
+bar {
+    status_command i3status | ~/.local/scripts/i3currentMedia/pystatus.py
+}
+```
+
+If you are using i3-gaps, it will probably contain the `status_command i3status` already. You just have to add the pipe and the python script path after. 
+
+* in `i3status.conf` file (create one if you don't have any -> read i3status doc for more information) set `output_format = "i3bar"` inside the 'general' configuration, like this:
+
+```
+general {
+    ...
+    output_format = "i3bar"
+    ...
+}
+```
+
+* Reload i3 configs (usually `Mod + Shift + R`, if you haven't changed it).
+
+Tip: If you are not sure how this whole thing works, you can comment your config files using `#` at the beginning of each line. This way it is easy to revert the changes.
+
+### Credits:
+Almost everything made by [@rpieja](https://github.com/rpieja/) i just modified some parts
+Script is based on sample wrapper commited on original i3status repository.
---
+++ pystatus.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import subprocess
+import os
+
+if sys.version_info[0] == 2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+dir_path=os.path.dirname(os.path.realpath(__file__))
+
+
+def get_status():
+    status = subprocess.check_output("playerctl metadata --format \"{{status}}\"", shell=True)
+    status=status.decode('utf-8')
+    return status
+
+def get_artist():
+    artist = subprocess.check_output("playerctl metadata --format \"{{artist}}\"", shell=True)
+    artist=artist.decode('utf-8')
+    return artist[:-1]
+
+def get_song():
+    title = subprocess.check_output("playerctl metadata --format \"{{title}}\"", shell=True)
+    title=title.decode('utf-8')
+    return title[:-1]
+
+
+def read_line():
+    """ Interrupted respecting reader for stdin. """
+    # try reading a line, removing any extra whitespace
+    try:
+        line = sys.stdin.readline().strip()
+        # i3status sends EOF, or an empty line
+        if not line:
+            sys.exit(3)
+        return line
+    # exit on ctrl-c
+    except KeyboardInterrupt:
+        sys.exit()
+
+def print_line(message):
+    """ Non-buffered printing to stdout. """
+    sys.stdout.write(message + '\n')
+    sys.stdout.flush()
+
+def get_governor():
+    with open('/sys/devices/platform/i5k_amb.0/temp4_input') as fp:
+        return fp.readlines()[0].strip()
+
+
+
+if __name__ == '__main__':
+    # Skip the first line which contains the version header.
+    print_line(read_line())
+
+    # The second line contains the start of the infinite array.
+    print_line(read_line())
+
+    while True:
+
+        line, prefix = read_line(), ''
+        # ignore comma at start of lines
+        if line.startswith(','):
+            line, prefix = line[1:], ','
+        if get_status() in ['Playing\n']:
+            j = json.loads(line)
+            # insert information into the start of the json, but could be anywhere
+            # CHANGE THIS LINE TO INSERT SOMETHING ELSE
+            j.insert(0, {'color' : '#9ec600', 'full_text' : '♪ %s - %s' % (get_artist(), get_song()) , 'name' : 'spotify'})
+            # and echo back new encoded json
+            print_line(prefix+json.dumps(j))
+        else:
+            j = json.loads(line)
+            print_line(prefix+json.dumps(j))
+            #print_line(json.dumps(j))
